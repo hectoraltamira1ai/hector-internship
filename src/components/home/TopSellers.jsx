9673a701b6a2c19cee405b3a3b8474302eb37ebe
Hector <hectoraltamira1ai@gmail.com>
2024-12-19 13:49:34 -0800

    re-formatted some comments

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
-
   // Initializing state to hold top sellers data
   const [topSellers, setTopSellers] = useState([]);
 
@@ -18,14 +17,15 @@ const TopSellers = () => {
     setTopSellers(response.data);
   };
 
-
   // useEffect hook to fetch data when the component mounts
   useEffect(() => {
     getTopSellerApiData(); // Calling the data fetching function
   }, []); // Empty dependency array means this runs once after the initial render
 
   return (
-    <section id="section-popular" className="pb-5"> {/* Main section for top sellers */}
+    <section id="section-popular" className="pb-5">
+      {" "}
+      {/* Main section for top sellers */}
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -34,54 +34,83 @@ const TopSellers = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-12"> {/* Column for the list of top sellers */}
-            {topSellers.length ? (  // Conditional rendering based on data availability (must not be 0)
+          <div className="col-md-12">
+            {" "}
+            {/* Column for the list of top sellers */}
+            {topSellers.length ? ( // Conditional rendering based on data availability (must not be 0)
               <ol className="author_list">
-                {topSellers.map((item, index) => ( // Mapping through top sellers data
-                  <li key={index}>  {/* Unique key for each list item ( 0-11)*/}
-                    <div className="author_list_pp">
-                      <Link to={`/author/${item.authorId}`}> {/* Link to author page */}
-                        <img
-                          className="lazy pp-author"  // Lazy loading class for images
-                          src={item.authorImage}
-                          alt=""
-                        />
-                        <i className="fa fa-check"></i>
-                      </Link>
-                    </div>
-                    <div className="author_list_info">
-                      <Link to={`/author/${item.authorId}`}>  {/* Link to author page */}
-                        {item.authorName}
-                      </Link>
-                      <span>{item.price} ETH</span>  {/* Displaying price in ETH */}
-                    </div>
-                  </li>
-                ))}
+                {topSellers.map(
+                  (
+                    item,
+                    index // Mapping through top sellers data
+                  ) => (
+                    <li key={index}>
+                      {" "}
+                      {/* Unique key for each list item ( 0-11)*/}
+                      <div className="author_list_pp">
+                        <Link to={`/author/${item.authorId}`}>
+                          {" "}
+                          {/* Link to author page */}
+                          <img
+                            className="lazy pp-author" // Lazy loading class for images
+                            src={item.authorImage}
+                            alt=""
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={`/author/${item.authorId}`}>
+                          {" "}
+                          {/* Link to author page */}
+                          {item.authorName}
+                        </Link>
+                        <span>{item.price} ETH</span>{" "}
+                        {/* Displaying price in ETH */}
+                      </div>
+                    </li>
+                  )
+                )}
               </ol>
-            ) : (  // Fallback rendering when no data is available (Skeleton State)
-              <ol className="author_list"> {/* Ordered list for loading skeletons */}
-                {new Array(12).fill(0).map((item, index) => (  // Creating 12 skeletons, filled with 0
-                  <li key={index}> {/* Unique key for each skeleton item */}
-                    <div className="author_list_pp">
-                      <Link to={``}> {/* Empty link for skeleton */}
-                        <Skeleton
-                          width="50px"
-                          height="50px"
-                          borderRadius="50%"
-                        />
-                        <i className="fa fa-check"></i>
-                      </Link>
-                    </div>
-                    <div className="author_list_info">
-                      <Link to={``}> {/* Empty link for skeleton */}
-                        <Skeleton width="100px" height="20px" />
-                      </Link>
-                      <span>
-                        <Skeleton width="40px" height="20px" /> {/* Skeleton for price */}
-                      </span>
-                    </div>
-                  </li>
-                ))}
+            ) : (
+              // Fallback rendering when no data is available (Skeleton State)
+              <ol className="author_list">
+                {" "}
+                {/* Ordered list for loading skeletons */}
+                {new Array(12).fill(0).map(
+                  (
+                    item,
+                    index // Creating 12 skeletons, filled with 0
+                  ) => (
+                    <li key={index}>
+                      {" "}
+                      {/* Unique key for each skeleton item */}
+                      <div className="author_list_pp">
+                        <Link to={``}>
+                          {" "}
+                          {/* Empty link for skeleton */}
+                          <Skeleton
+                            width="50px"
+                            height="50px"
+                            borderRadius="50%"
+                          />
+                          <i className="fa fa-check"></i>
+                        </Link>
+                      </div>
+                      <div className="author_list_info">
+                        <Link to={``}>
+                          {" "}
+                          {/* Empty link for skeleton */}
+                          <Skeleton width="100px" height="20px" />
+                        </Link>
+                        <span>
+                          <Skeleton width="40px" height="20px" />{" "}
+                          {/* Skeleton for price */}
+                        </span>
+                      </div>
+                    </li>
+                  )
+                )}
               </ol>
             )}
           </div>
