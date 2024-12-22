31921a8d15f0e708ff6fcd439a84a5bad6145959
Hector <hectoraltamira1ai@gmail.com>
2024-12-21 19:57:15 -0800

    completed Explore Items feature

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,26 +2,30 @@ import React, { useEffect } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
+// Using useEffect to ensure that the page scrolls to the top when the component
+// is mounted. 
 const Explore = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, []); //empty dependency ensures that the effect will only run once.
 
   return (
-    <div id="wrapper">
+    <div id="wrapper"> {/* Wrapper div for the entire page */}
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
 
+
+        {/* Subheader Section */}
         <section
           id="subheader"
           className="text-light"
-          style={{ background: `url("${SubHeader}") top` }}
+          style={{ background: `url("${SubHeader}") top` }} //url to change background image
         >
           <div className="center-y relative text-center">
             <div className="container">
               <div className="row">
                 <div className="col-md-12 text-center">
-                  <h1>Explore</h1>
+                  <h1>Explore</h1> {/* Page Title */}
                 </div>
                 <div className="clearfix"></div>
               </div>
@@ -29,10 +33,12 @@ const Explore = () => {
           </div>
         </section>
 
+
+        {/* Main Content Section -starts with 2 rows of 4 each "more" will add 4 more until 8 total is reached*/}
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <ExploreItems />
+              <ExploreItems /> {/* Child component that renders the items to explore */}
             </div>
           </div>
         </section>
@@ -41,4 +47,6 @@ const Explore = () => {
   );
 };
 
+
+// Exporting the component for use in other parts of the application
 export default Explore;
